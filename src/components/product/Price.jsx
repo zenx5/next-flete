04e0bca6fe9db68e55102491a335796ded92f450
Octavio Martinez <omartinez1618@gmail.com>
2023-07-19 23:32:34 -0400

    Price template

---
+++ src/components/product/Price.jsx
@@ -0,0 +1,9 @@
+export default function Price({ regularPrice, price }) {
+
+    const mainPrice = price ? price : regularPrice;
+
+    return <span className="mt-2">
+        <small className="leading-none text-xs line-through text-gray-500">{mainPrice!==regularPrice ? regularPrice : '-'}</small>
+        <p className="leading-none text-lg text-center md:text-left font-medium text-gray-900">{mainPrice}</p>
+    </span>
+}
