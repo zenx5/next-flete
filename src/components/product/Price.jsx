c69ed6b91698170077f0c9d07d29327480f20dc5
Octavio Martinez <omartinez1618@gmail.com>
2023-07-20 01:13:40 -0400

    align price in component

--- src/components/product/Price.jsx
+++ src/components/product/Price.jsx
@@ -4,6 +4,6 @@ export default function Price({ regularPrice, price }) {
 
     return <span className="mt-2">
         <small className="leading-none text-xs line-through text-gray-500">{mainPrice!==regularPrice ? regularPrice : '-'}</small>
-        <p className="leading-none text-lg text-center md:text-left font-medium text-gray-900">{mainPrice}</p>
+        <p className="leading-none text-lg text-left font-medium text-gray-900">{mainPrice}</p>
     </span>
 }
