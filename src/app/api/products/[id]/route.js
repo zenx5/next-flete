08bf62e633497908f2831546f33531287169fdcf
Octavio Martinez <omartinez1618@gmail.com>
2023-07-20 09:04:50 -0400

    added realtedProducts field to response of api for unique product

--- src/app/api/products/[id]/route.js
+++ src/app/api/products/[id]/route.js
@@ -6,6 +6,10 @@ import { products } from '../products.mockup';
 export async function GET(request, { params }) {
     const { id } = params
     const product = products.find( product => product.id==id )
-    if ( product ) return NextResponse.json({ code:0, data:product })
+
+    if ( product ) return NextResponse.json({ code:0, data:{
+        ...product,
+        relatedProducts: products.filter( _product => _product.id!=id ).slice(0,3)
+    } })
     return NextResponse.json({ code:1, data:null })
 }
