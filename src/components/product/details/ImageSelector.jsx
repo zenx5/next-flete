9564e23030526c1df6febd0c27593f366bea389f
Octavio Martinez <omartinez1618@gmail.com>
2023-07-20 10:41:59 -0400

    hide control change of image when the array images has size 1

--- src/components/product/details/ImageSelector.jsx
+++ src/components/product/details/ImageSelector.jsx
@@ -20,7 +20,7 @@ export default function ImageSelector({ images }) {
             {/* Image selector */}
             <div className="mx-auto mt-6 hidden w-full max-w-2xl sm:block lg:max-w-none">
                 <Tab.List className="grid grid-cols-4 gap-6">
-                    {images?.map((image) => (
+                    {images.length>1 && images?.map((image) => (
                         <Tab
                             key={image.id}
                             className="relative flex h-24 cursor-pointer items-center justify-center rounded-md bg-white text-sm font-medium uppercase text-gray-900 hover:bg-gray-50 focus:outline-none focus:ring focus:ring-opacity-50 focus:ring-offset-4"
@@ -34,7 +34,7 @@ export default function ImageSelector({ images }) {
                                     <span
                                         className={classNames(
                                             selected ? 'ring-indigo-500' : 'ring-transparent',
-                                            'pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2'
+                                            'pointer-events-none absolute inset-0 rounded-md ring-2 ring-offset-2 shadow-lg'
                                         )}
                                         aria-hidden="true"
                                     />
@@ -48,17 +48,17 @@ export default function ImageSelector({ images }) {
             <Tab.Panels className="aspect-h-1 aspect-w-1 w-full">
                 {images?.map((image, index) => (
                     <Tab.Panel key={image.id} className="flex flex-row items-center">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={"w-6 h-6 md:invisible " + (index==0 ? "text-gray-500" : "cursor-pointer text-blue-500")} onClick={handlerClickChangeImage(index,-1)}>
+                        {images.length && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={"w-6 h-6 md:invisible " + (index==0 ? "text-gray-500" : "cursor-pointer text-blue-500")} onClick={handlerClickChangeImage(index,-1)}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-                        </svg>
+                        </svg>}
                         <img
                             src={image.src}
                             alt={image.alt}
-                            className="h-full w-full object-cover object-center sm:rounded-lg"
+                            className="h-full w-full object-cover object-center sm:rounded-lg shadow-xl"
                         />
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={"w-6 h-6 md:invisible " + ((index==images.length-1) ? "text-gray-500" : "cursor-pointer text-blue-500")} onClick={handlerClickChangeImage(index,1)}>
+                        {images.length && <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className={"w-6 h-6 md:invisible " + ((index==images.length-1) ? "text-gray-500" : "cursor-pointer text-blue-500")} onClick={handlerClickChangeImage(index,1)}>
                             <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-                        </svg>
+                        </svg>}
 
                     </Tab.Panel>
                 ))}
