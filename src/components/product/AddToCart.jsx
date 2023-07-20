9784a7b1a83452a4013258e839e91ce94c201889
Octavio Martinez <omartinez1618@gmail.com>
2023-07-20 09:08:10 -0400

    added state 'count' for show items quantity in the cart

--- src/components/product/AddToCart.jsx
+++ src/components/product/AddToCart.jsx
@@ -1,15 +1,17 @@
 "use client";
 import { ShoppingBagIcon } from '@heroicons/react/24/outline'
+import { useState } from 'react';
 
-export default function AddToCart({ id, classNameContainer, label, classNameIcon }) {
-
+export default function AddToCart({ id, enableCount, classNameContainer, label=null, classNameIcon }) {
+    const [count, setCount] = useState(2)
     const handlerClick_AddToCart = (event) => {
         event.preventDefault()
         console.log(`handlerClick, AddToCart ${id}`)
         
     }
     
-    return <button className={classNameContainer} href="#add-to-cart" onClick={handlerClick_AddToCart}>
+    return <button className={classNameContainer+" flex-row flex"} href="#add-to-cart" onClick={handlerClick_AddToCart}>
         { label ? label : <ShoppingBagIcon className={classNameIcon} aria-hidden="true" />}
+        { (enableCount && count>0 ) && <span>({count})</span> }
     </button>
 }
