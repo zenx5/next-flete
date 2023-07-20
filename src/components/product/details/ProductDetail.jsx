c18abb93cc6c4889e73032e2ca3049ed53792e95
Octavio Martinez <omartinez1618@gmail.com>
2023-07-20 09:09:22 -0400

    show related products in details product view

--- src/components/product/details/ProductDetail.jsx
+++ src/components/product/details/ProductDetail.jsx
@@ -50,7 +50,7 @@ function classNames(...classes) {
 	return classes.filter(Boolean).join(' ')
 }
 
-export default function ProductDetail({ product, relatedProducts }) {
+export default function ProductDetail({ product }) {
 	const [open, setOpen] = useState(false)
 	//const [selectedColor, setSelectedColor] = useState(product.colors[0])
 
@@ -59,7 +59,7 @@ export default function ProductDetail({ product, relatedProducts }) {
 			<main className="mx-auto max-w-7xl sm:px-6 sm:pt-16 lg:px-8">
 				<div className="mx-auto max-w-2xl lg:max-w-none">
 					{/* Product */}
-					<div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
+					<div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 border-b-2 border-gray-400 pb-3">
 						{/* Image gallery */}
 						<ImageSelector images={product?.images} />
 
@@ -84,7 +84,8 @@ export default function ProductDetail({ product, relatedProducts }) {
 								<div className="mt-10 flex gap-2">
 									<AddToCart
 										id={product.id}
-										label="Agregar al Carrito"
+										enableCount
+										classNameIcon="text-blue-400 w-5"
 										classNameContainer="p-3 text-blue-400 border-2 border-blue-400 rounded-md hover:text-blue-500 hover:bg-blue-100"
 									/>
 									<BuyNow
@@ -95,7 +96,7 @@ export default function ProductDetail({ product, relatedProducts }) {
 
 							<section aria-labelledby="details-heading" className="mt-12">
 								<h2 id="details-heading" className="sr-only">
-									Additional details
+									Detalles Adicionales
 								</h2>
 
 								<div className="divide-y divide-gray-200 border-t">
@@ -143,7 +144,6 @@ export default function ProductDetail({ product, relatedProducts }) {
 							</section>
 						</div>
 					</div>
-					<tr />
 
 					{ product?.relatedProducts?.length>0 && <section aria-labelledby="related-heading" className="mt-10 border-t border-gray-200 px-4 py-16 sm:px-0">
 						<h2 id="related-heading" className="text-xl font-bold text-gray-900">
@@ -151,26 +151,26 @@ export default function ProductDetail({ product, relatedProducts }) {
 						</h2>
 
 						<div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-							{relatedProducts?.map((product) => (
+							{product.relatedProducts?.map((product) => (
 								<div key={product.id}>
 									<div className="relative">
 										<div className="relative h-72 w-full overflow-hidden rounded-lg">
 											<img
-												src={product.imageSrc}
-												alt={product.imageAlt}
+												src={product.images[0].src}
+												alt={product.images[0].alt}
 												className="h-full w-full object-cover object-center"
 											/>
 										</div>
 										<div className="relative mt-4">
 											<h3 className="text-sm font-medium text-gray-900">{product.name}</h3>
-											<p className="mt-1 text-sm text-gray-500">{product.color}</p>
+											{/* <p className="mt-1 text-sm text-gray-500">{product.color}</p> */}
 										</div>
 										<div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
 											<div
 												aria-hidden="true"
 												className="absolute inset-x-0 bottom-0 h-36 bg-gradient-to-t from-black opacity-50"
 											/>
-											<p className="relative text-lg font-semibold text-white">{product.price}</p>
+											<Price price={product.price} regularPrice={product.regularPrice}/>
 										</div>
 									</div>
 									<div className="mt-6">
