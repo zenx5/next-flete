6916980b6aae5f5681477de7fd297988f8f1fed0
Octavio Martinez <omartinez1618@gmail.com>
2023-07-20 14:31:00 -0400

    added count in button 'Add to card'

--- src/components/product/AddToCart.jsx
+++ src/components/product/AddToCart.jsx
@@ -1,17 +1,34 @@
 "use client";
+import Cart from '@/tools/cart.fake';
 import { ShoppingBagIcon } from '@heroicons/react/24/outline'
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
-export default function AddToCart({ id, enableCount, classNameContainer, label=null, classNameIcon }) {
-    const [count, setCount] = useState(2)
+
+export default function AddToCart({ id, enableCount, classNameContainer, label=null, classNameIcon, badge }) {
+    const [count, setCount] = useState(0)
+
+    useEffect(()=>{
+        const value = Cart.quantity(id)
+        if( value ) {
+            setCount(prev => value)
+        }
+    },[id, setCount])
+
     const handlerClick_AddToCart = (event) => {
         event.preventDefault()
         console.log(`handlerClick, AddToCart ${id}`)
-        
+        Cart.add(id)
+        const value = Cart.quantity(id)
+        if( value ) {
+            setCount(prev => value)
+        }
     }
     
-    return <button className={classNameContainer+" flex-row flex"} href="#add-to-cart" onClick={handlerClick_AddToCart}>
-        { label ? label : <ShoppingBagIcon className={classNameIcon} aria-hidden="true" />}
-        { (enableCount && count>0 ) && <span>({count})</span> }
-    </button>
+    return <>
+        <button className={classNameContainer+" flex-row flex"} href="#add-to-cart" onClick={handlerClick_AddToCart}>
+            { label ? label : <ShoppingBagIcon className={classNameIcon} aria-hidden="true" />}
+            { ( enableCount && !badge && count>0 ) && <span>({count})</span> }
+        </button>
+    </>
+
 }
