eebdc8c427985906ebd5f69b7db9b5e4e8c6fb3a
Octavio Martinez <omartinez1618@gmail.com>
2023-07-20 10:42:36 -0400

    convert server component to client component

--- src/components/product/details/ProductDetail.jsx
+++ src/components/product/details/ProductDetail.jsx
@@ -1,58 +1,11 @@
-"use client";
-import { Fragment, useState } from 'react'
-import { Dialog, Disclosure, Popover, RadioGroup, Tab, Transition } from '@headlessui/react'
-import {
-	Bars3Icon,
-	HeartIcon,
-	MagnifyingGlassIcon,
-	MinusIcon,
-	PlusIcon,
-	ShoppingBagIcon,
-	UserIcon,
-	XMarkIcon,
-} from '@heroicons/react/24/outline'
-import { StarIcon } from '@heroicons/react/20/solid'
 import ImageSelector from './ImageSelector';
 import AddToCart from '../AddToCart'
 import BuyNow from '../BuyNow'
 import Price from '../Price';
+import Details from './Details';
 
-
-
-const footerNavigation = {
-	products: [
-		{ name: 'Bags', href: '#' },
-		{ name: 'Tees', href: '#' },
-		{ name: 'Objects', href: '#' },
-		{ name: 'Home Goods', href: '#' },
-		{ name: 'Accessories', href: '#' },
-	],
-	company: [
-		{ name: 'Who we are', href: '#' },
-		{ name: 'Sustainability', href: '#' },
-		{ name: 'Press', href: '#' },
-		{ name: 'Careers', href: '#' },
-		{ name: 'Terms & Conditions', href: '#' },
-		{ name: 'Privacy', href: '#' },
-	],
-	customerService: [
-		{ name: 'Contact', href: '#' },
-		{ name: 'Shipping', href: '#' },
-		{ name: 'Returns', href: '#' },
-		{ name: 'Warranty', href: '#' },
-		{ name: 'Secure Payments', href: '#' },
-		{ name: 'FAQ', href: '#' },
-		{ name: 'Find a store', href: '#' },
-	],
-}
-
-function classNames(...classes) {
-	return classes.filter(Boolean).join(' ')
-}
 
 export default function ProductDetail({ product }) {
-	const [open, setOpen] = useState(false)
-	//const [selectedColor, setSelectedColor] = useState(product.colors[0])
 
 	return (
 		<div>
@@ -100,46 +53,7 @@ export default function ProductDetail({ product }) {
 								</h2>
 
 								<div className="divide-y divide-gray-200 border-t">
-									{product?.details?.map((detail) => (
-										<Disclosure as="div" key={detail.name}>
-											{({ open }) => (
-												<>
-													<h3>
-														<Disclosure.Button className="group relative flex w-full items-center justify-between py-6 text-left">
-                              <span
-								  className={classNames(
-									  open ? 'text-indigo-600' : 'text-gray-900',
-									  'text-sm font-medium'
-								  )}
-							  >
-                                {detail.name}
-                              </span>
-															<span className="ml-6 flex items-center">
-                                {open ? (
-									<MinusIcon
-										className="block h-6 w-6 text-indigo-400 group-hover:text-indigo-500"
-										aria-hidden="true"
-									/>
-								) : (
-									<PlusIcon
-										className="block h-6 w-6 text-gray-400 group-hover:text-gray-500"
-										aria-hidden="true"
-									/>
-								)}
-                              </span>
-														</Disclosure.Button>
-													</h3>
-													<Disclosure.Panel as="div" className="prose prose-sm pb-6">
-														<ul role="list">
-															{detail.items.map((item) => (
-																<li key={item}>{item}</li>
-															))}
-														</ul>
-													</Disclosure.Panel>
-												</>
-											)}
-										</Disclosure>
-									))}
+									{product?.details?.map((detail) => <Details key={detail.name} name={detail.name} items={detail.items}/> )}
 								</div>
 							</section>
 						</div>
@@ -153,17 +67,22 @@ export default function ProductDetail({ product }) {
 						<div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
 							{product.relatedProducts?.map((product) => (
 								<div key={product.id}>
-									<div className="relative">
-										<div className="relative h-72 w-full overflow-hidden rounded-lg">
+									<div className="relative  pb-2 border-b-2 border-gray-300 md:border-none">
+										<div className="relative h-72 w-full overflow-hidden rounded-lg ">
 											<img
 												src={product.images[0].src}
 												alt={product.images[0].alt}
 												className="h-full w-full object-cover object-center"
 											/>
 										</div>
-										<div className="relative mt-4">
-											<h3 className="text-sm font-medium text-gray-900">{product.name}</h3>
-											{/* <p className="mt-1 text-sm text-gray-500">{product.color}</p> */}
+										<div className="relative mt-4 flex flex-row gap-2 items-center md:flex-col md:items-start">
+											<h3 className="text-sm font-medium text-gray-900 w-1/2 md:w-full">{product.name}</h3>
+											<AddToCart
+												id={product.id}
+												label="Agregar al Carrito"
+												classNameContainer="p-3 w-1/2 md:w-full text-blue-400 border-2 border-blue-400 rounded-md hover:text-blue-500 hover:bg-blue-100"
+											/>
+											
 										</div>
 										<div className="absolute inset-x-0 top-0 flex h-72 items-end justify-end overflow-hidden rounded-lg p-4">
 											<div
@@ -172,13 +91,6 @@ export default function ProductDetail({ product }) {
 											/>
 											<Price price={product.price} regularPrice={product.regularPrice}/>
 										</div>
-									</div>
-									<div className="mt-6">
-										<AddToCart
-											id={product.id}
-											label="Agregar al Carrito"
-											classNameContainer="p-3 w-full text-blue-400 border-2 border-blue-400 rounded-md hover:text-blue-500 hover:bg-blue-100"
-										/>
 									</div>
 								</div>
 							))}
