685f1d381eb436fb56518430b9fd57f71f6c4b58
Octavio Martinez <omartinez1618@gmail.com>
2023-07-23 22:11:17 -0400

    force cache

--- src/app/productos/[id]/page.js
+++ src/app/productos/[id]/page.js
@@ -4,7 +4,7 @@ import ProductDetail from '@/components/product/details/ProductDetail'
 export default async function Products({ params }) {
     const { id } = params;
     
-    const response = await fetch(`${process.env.API_URL}/products/${id}`)
+    const response = await fetch(`${process.env.API_URL}/products/${id}`, { cache:'force-cache' })
     const { code, data:product } = await response.json()
     
     return (
