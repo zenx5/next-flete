61b473d43cb9048ee90770cdb60d7c3aacbc3637
Octavio Martinez <omartinez1618@gmail.com>
2023-07-30 18:27:04 -0400

    change route name Carrito >> carrito

---
+++ src/app/carrito/layout.js
@@ -0,0 +1,11 @@
+import CustomFooter from "@/components/CustomFooter";
+import CustomHeader from "@/components/CustomHeader";
+
+
+export default function Layout({ children }) {
+
+    return <>
+        { children }
+    </>
+
+}
