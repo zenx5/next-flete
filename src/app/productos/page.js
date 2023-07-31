a9f10ea33fc37af7b374108142620ed5ae641fc0
Octavio Martinez <omartinez1618@gmail.com>
2023-07-31 13:16:59 -0400

    change styles

--- src/app/productos/page.js
+++ src/app/productos/page.js
@@ -5,19 +5,19 @@ import Card from '@/components/Card';
 export default async function Products() {
     const response = await fetch(`${process.env.API_URL}/products`, { cache:'force-cache' })
     const { code, data:products } = await response.json()
-    const isActive = "border-b-2 border-b-blue-500"
+    const isActive = "border-b-2 border-b-white"
 
 
     return (
         <main className="flex min-h-screen flex-col bg-slate-100">
-            <form className="flex flex-col md:flex-row justify-between items-center mb-4 bg-white p-3 gap-3">
+            <form className="flex flex-col md:flex-row justify-between items-center mb-4 bg-black opacity-90 p-3 gap-3">
                 <ul className="flex flex-row justify-between w-full md:w-1/3 p-0 m-0">
-                    <li><a className={`text-blue-500 hover:bg-blue-100 p-2 pb-0 ${isActive}`} href="#">Ascenso 1</a></li>
-                    <li><a className="text-blue-500 hover:bg-blue-100 p-2 pb-0" href="#">Ascenso 2</a></li>
-                    <li><a className="text-blue-500 hover:bg-blue-100 p-2 pb-0" href="#">Ascenso 3</a></li>
+                    <li><a className={`text-white p-2 pb-0 ${isActive}`} href="#">Ascenso 1</a></li>
+                    <li><a className="text-white p-2 pb-0" href="#">Ascenso 2</a></li>
+                    <li><a className="text-white p-2 pb-0" href="#">Ascenso 3</a></li>
                 </ul>
                 <InputSearch />
-                <select className="w-full md:w-1/3 p-2 border-2 border-blue-300 hover:border-blue-500 bg-white text-blue-500 rounded-md">
+                <select className="w-full md:w-1/3 p-2 border-2 border-white bg-white text-black rounded-md">
                     <option>Promoción 1</option>
                     <option>Promoción 2</option>
                     <option>Promoción 3</option>
--- src/components/InputSearch.jsx
+++ src/components/InputSearch.jsx
@@ -1,8 +1,8 @@
 export default function InputSearch(props) {
-    return <span className="border border-blue-400 p-1 px-2 mx-auto flex flex-row items-center w-full md:w-fit rounded-lg">
+    return <span className="border bg-white p-1 px-2 mx-auto flex flex-row items-center w-full md:w-fit rounded-lg">
         <input type="text" className="border-none outline-none p-1 w-full" {...props} />
         <button>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6  text-blue-400">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-black">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>
         </button>
