6b826486197e91305df62c3ac998f8940ac94d14
Octavio Martinez <omartinez1618@gmail.com>
2023-08-11 00:03:21 -0400

    agregado hook para obtener los productos del carrito

---
+++ src/tools/useProducts.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from "react"
+import Cart from "./cart.fake"
+
+export default function useProducts(){
+    const [loaded, setLoaded] = useState( false )
+    const [products, setProducts] = useState([])
+    const [quantity, setQuantity] = useState(0)
+
+    useEffect(()=>{
+        if( products.length===0 && !loaded ) {
+            console.log('useEffect');
+            (async ()=>{
+                const { index } = Cart.get()
+                const response = await loadProducts(index)
+                setProducts( prev => response.map( item => ({
+                    ...item,
+                    quantity: Cart.quantity( item.id )
+                })) )
+                setQuantity( prev => response.length )
+                setLoaded( true )
+            })()
+        }
+    },[loaded, products])
+
+    const loadProducts = async (list) => {
+        const response = await fetch('/api/cart', {
+          method:'post',
+          body:JSON.stringify({
+            list
+          })
+        })
+        const { code, data } = await response.json()
+        return code==0 ? data : []
+    }
+
+    const updateQuantity = id => value => {
+        const oldQuantity = Cart.quantity( id )
+        if( value === 0 ) {
+            Cart.remove(id, oldQuantity)
+        } else {
+            Cart.add(id, value - oldQuantity)
+        }
+        setLoaded( false )
+    }
+
+    return {products, updateQuantity, quantity}
+}
