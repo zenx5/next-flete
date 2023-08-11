382b828a49d1c65404188a239b6f5b20b0f2f382
Octavio Martinez <omartinez1618@gmail.com>
2023-08-11 00:02:23 -0400

    ajuste en componente para reutilizarlo

--- src/components/cart/ItemProductInCart.jsx
+++ src/components/cart/ItemProductInCart.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react"
 import QuantityBubble from "../QuantityBubble"
 
-export default function ItemProducInCart({ product, quantity, onChange, onRemove }) {
+export default function ItemProducInCart({ product, quantity, onChange, onRemove, compact }) {
 
     const handlerChangeQuantity = (value) => {
         if( onChange ) onChange(value)
     }
 
-    return <li className="flex space-x-6 py-6">
-        <img
+    return <li className={`flex space-x-6 ` + (compact ? 'py-2' : 'py-6')}>
+        { !compact && <img
             src={product.images?.at(0)?.src}
             alt={product.images?.at(0)?.alt}
             className="h-24 w-24 flex-none rounded-md bg-gray-100 object-cover object-center"
-        />
+        />}
         <div className="flex-auto">
             <div className="space-y-1 sm:flex sm:items-start sm:justify-between sm:space-x-6">
                 <div className="flex-auto space-y-1 text-sm font-medium">
@@ -21,11 +21,11 @@ export default function ItemProducInCart({ product, quantity, onChange, onRemove
                     </h3>
                     <p className="text-gray-900">{product.price}</p>
                 </div>
-                <div className="flex justify-end space-x-4">
-                    <QuantityBubble min={0} onChange={handlerChangeQuantity} value={quantity}/>
-                    <div className="flex border-l border-gray-300 pl-4">
+                <div className={compact ? "flex flex-col": "flex justify-end space-x-4"}>
+                    { quantity!==undefined && <QuantityBubble min={0} onChange={handlerChangeQuantity} value={quantity}/>}
+                    { onRemove && <div className={ compact ? "flex pl-4" : "flex border-l border-gray-300 pl-4"}>
                         <button type="button" className="text-sm font-medium text-black font-weight" onClick={onRemove}>Remove</button>
-                    </div>
+                    </div>}
                 </div>
             </div>
         </div>
