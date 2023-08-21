4c3547aa37abd51acd2697e6c92db9c8ccf00910
Octavio Martinez <omartinez1618@gmail.com>
2023-08-21 12:21:40 -0400

    animacion de acordion

--- src/app/confirmar-compra/Details/ActionsDetails.js
+++ src/app/confirmar-compra/Details/ActionsDetails.js
@@ -1,15 +1,29 @@
 "use client";
 
-import { Disclosure } from "@headlessui/react";
+import { Disclosure, Transition } from "@headlessui/react";
 
 export default function ActionsDetails({ label, children, ...props }) {
+    const handlerClick = (e) => {
+        if( props?.onClick ) props.onClick(e)
+    }
+
     return <Disclosure as="div"
+        defaultOpen={props?.defaultopen ?? false}
         key={label}>
         {({ open }) => (<>
-            <h3 {...props}><Disclosure.Button>{label}</Disclosure.Button></h3>
-            <Disclosure.Panel as="div">
-                {children}
-            </Disclosure.Panel>
+            <h3 {...props}><Disclosure.Button onClick={handlerClick}>{label}</Disclosure.Button></h3>
+            <Transition
+                enter="transition duration-300 ease-out"
+                enterFrom="transform scale-50 opacity-0"
+                enterTo="transform scale-100 opacity-100"
+                leave="transition duration-100 ease-out"
+                leaveFrom="transform scale-100 opacity-100"
+                leaveTo="transform scale-50 opacity-0"
+            >
+                <Disclosure.Panel as="div">
+                    {children}
+                </Disclosure.Panel>
+            </Transition>
         </>)}
     </Disclosure>
 }
