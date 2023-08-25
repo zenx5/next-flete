69b85ec636473f07e8391cb6cbad6a1e01a94da4
Octavio Martinez <omartinez1618@gmail.com>
2023-08-25 10:16:46 -0400

    habilitada server actions

--- next.config.js
+++ next.config.js
@@ -1,11 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {env: {
+const nextConfig = {
+    env: {
         DB_URI: "mongodb://localhost:27017/ecommercev1",
         API_URL: "http://localhost:3000/api",
     },
     images: {
         // remotePatterns: [{ protocol: "https:", hostname: "**" }],
         domains: ["localhost", "fastly.picsum.photos", "picsum.photos", "tailwindui.com", "detallesorballo.com"],
-    },}
+    },
+    experimental: {
+        serverActions: true,
+    }
+}
 
 module.exports = nextConfig
---
+++ src/tools/actions.js
@@ -0,0 +1,55 @@
+"use server";
+import { cookies } from "next/headers"
+
+
+// Definir estructura de Cookies de usuario
+const userCookie = {
+    name: "",
+    email: "",
+    token: "",
+    id: "",
+    expirationDate: "",
+};
+
+export const execAction = async (action) => {
+    switch (action) {
+        case "logout":
+            await removeUser();
+            break;
+        default:
+            break;
+    }
+}
+
+
+export const isAuthenticated = () => {
+    const user = cookies().get("user");
+    if (user?.value) {
+        const { expirationDate } = JSON.parse(user.value);
+        if (expirationDate > Date.now()) {
+            return true;
+        }
+    }
+    return false;
+}
+
+
+export const getUser = () => {
+    const user = cookies().get("user");
+    return user?.value ? JSON.parse(user.value) : false;
+}
+
+export const setUser = (user) => {
+    const expires = user.expirationDate ? user.expirationDate : Date.now() + 24 * 60 * 60 * 1000;
+    cookies().set("user", JSON.stringify({
+        ...userCookie,
+        ...user,
+        expirationDate: expires
+    }), {
+        expires: expires
+    });
+}
+
+export const removeUser = async () => {
+    await cookies().delete("user");
+}
