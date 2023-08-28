cfe6e2aa76b3dca5c3587ce1dd3e0fb44e3c2406
Octavio Martinez <omartinez1618@gmail.com>
2023-08-28 12:11:52 -0400

    mockups

---
+++ src/tools/mockup/users.mockup.js
@@ -0,0 +1,26 @@
+export const users = [
+    {
+        id: 1,
+        name: 'John Doe',
+        email: 'jdoe@mail.com',
+        password: '123456',
+        birthdate: '1990-01-01',
+        phone: '1234567890',
+        phoneFamily: '1234567890',
+        grade: 'Grado 1',
+        academic: 'Aviaciòn Militar Bolivariana',
+        nextGrade: 'Grado 2',
+    },
+    {
+        id: 2,
+        name: 'Jane Doe',
+        email: 'jandoe@mail.com',
+        password: '123456',
+        birthdate: '1990-01-01',
+        phone: '1234567890',
+        phoneFamily: '1234567890',
+        grade: 'Grado 1',
+        academic: 'Aviaciòn Militar Bolivariana',
+        nextGrade: 'Grado 2',
+    }
+]
