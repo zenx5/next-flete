07f3767c3c4e0b708375e0c1ae8769cff370415c
sibelius303 <74629371+sibelius303@users.noreply.github.com>
2023-09-01 14:36:04 -0300

    correciones en las cards de productos

--- src/components/product/Price.jsx
+++ src/components/product/Price.jsx
@@ -3,7 +3,6 @@ export default function Price({ regularPrice, price }) {
     const mainPrice = price ? price : regularPrice;
 
     return <span className="mt-2">
-        <small className="leading-none text-xs line-through text-gray-500">{mainPrice!==regularPrice ? regularPrice : '-'}</small>
-        <p className="leading-none text-lg text-left font-medium text-gray-900">{mainPrice}</p>
+        <p className="leading-none text-sm text-left w-auto font-medium text-gray-900">{mainPrice}</p>
     </span>
 }
