3b84c683cdedcc3634d12ddc6c7ca83d0c6125a5
Octavio Martinez <omartinez1618@gmail.com>
2023-10-02 21:31:43 -0400

    detalle de las auctions

--- src/app/productos/[id]/page.js
+++ src/app/productos/[id]/page.js
@@ -1,5 +1,6 @@
 import ProductDetail from '@/components/product/details/ProductDetail'
 import { products } from '@/tools/mockup/products.mockup';
+import { auctions } from '@/tools/mockup/auctions.mockup';
 
 export default async function Products({ params }) {
     const { id } = params;
@@ -9,7 +10,10 @@ export default async function Products({ params }) {
     const product = products.find( product => product.id == id )
     return (
         <main className="flex min-h-screen flex-col">
-            { code==0 && <ProductDetail product={product} />}
+            { code==0 && <ProductDetail product={{
+                ...product,
+                auctions: product.auctions.map( id => auctions.find( auction => auction.id===id )).slice(0,3)
+            }} />}
         </main>
     )
 }
