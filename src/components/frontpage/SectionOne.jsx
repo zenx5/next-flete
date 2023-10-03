0174e86e95384c0f525840eab1aa0b0089ebcc41
MarichalesDev <marichalesdev@gmail.com>
2023-10-03 12:24:34 -0400

    Change the default frontpage

---
+++ src/components/frontpage/SectionOne.jsx
@@ -0,0 +1,11 @@
+export default function SectionOne() {
+
+	return (
+    <div class="flex items-center justify-evenly flex-col md:flex-row bg-blue-900 text-white p-3 md:p-5">
+    <div className="text-lg md:text-xl font-medium md:font-bold">4.5 MILLONES CLIENTES DE ENVÍO</div>
+    <div className="text-lg md:text-xl font-medium md:font-bold">988,000 TRANSPORTISTAS</div>
+    <div className="text-lg md:text-xl font-medium md:font-bold">6.7 MILLONES DE ANUNCIOS</div>
+    </div>
+    )
+
+}
