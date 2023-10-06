c7de03310bc4625cedf8c11a44e76016f28fb712
Octavio Martinez <omartinez1618@gmail.com>
2023-10-06 17:36:12 -0400

    product details views

--- src/app/productos/[id]/page.js
+++ src/app/productos/[id]/page.js
@@ -4,16 +4,8 @@ import { auctions } from '@/tools/mockup/auctions.mockup';
 
 export default async function Products({ params }) {
     const { id } = params;
-    // const response = await fetch(`${process.env.API_URL}/products/${id}`, { cache:'force-cache' })
-    // const { code, data:product } = await response.json()
-    const code = 0
-    const product = products.find( product => product.id == id )
-    return (
-        <main className="flex min-h-screen flex-col">
-            { code==0 && <ProductDetail product={{
-                ...product,
-                auctions: product.auctions.map( id => auctions.find( auction => auction.id===id )).slice(0,3)
-            }} />}
+
+    return <main className="flex min-h-screen flex-col">
+            <ProductDetail productId={id} />
         </main>
-    )
 }
--- src/components/product/details/ProductDetail.jsx
+++ src/components/product/details/ProductDetail.jsx
@@ -1,3 +1,4 @@
+"use client";
 import ImageSelector from './ImageSelector';
 import AddToCart from '../AddToCart'
 import AuctionUp from '../AuctionUp'
@@ -7,6 +8,8 @@ import CardRelatedProduct from '../../CardRelatedProduct';
 import HeartButton from '../HeartButton';
 import ShareButton from '../ShareButton';
 import ShareModal from './ShareModal';
+import { useState, useEffect } from 'react';
+import { onSnap } from '@/tools/firebase/actions';
 
 const opacities = [
 	"opacity-80",
@@ -15,7 +18,14 @@ const opacities = [
 ]
 
 
-export default function ProductDetail({ product }) {
+export default function ProductDetail({ productId }) {
+	const [product, setProduct] = useState({})
+
+	useEffect(()=>{
+		onSnap(process.env.NEXT_PUBLIC_ENTITY_PRODUCT_NAME, (result)=>{
+			setProduct( prev => result )
+		}, productId)
+	},[productId])
 
 	const hideEmail = (email) => {
 		if( !email ) return ""
@@ -35,89 +45,85 @@ export default function ProductDetail({ product }) {
 			.join("@")
 	}
 
-	return (
-		<div>
-			<main className="mx-auto max-w-7xl bg-white sm:px-12 sm:pt-20 lg:px-16">
-				<div className="mx-auto max-w-2xl lg:max-w-none">
-					{/* Product */}
-					<div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 border-b-2 border-gray-400 pb-3">
-						{/* Image gallery */}
-						<ImageSelector images={product?.images} />
+	return <main className="mx-auto max-w-7xl bg-white sm:px-12 sm:pt-20 lg:px-16">
+		<div className="mx-auto max-w-2xl lg:max-w-none">
+			{/* Product */}
+			<div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8 border-b-2 border-gray-400 pb-3">
+				{/* Image gallery */}
+				<ImageSelector images={product?.images} />
 
-						{/* Product info */}
-						<div className="mt-4 md:mt-0 px-5 pt-8 bg-white">
-							<h1 className="text-3xl font-bold tracking-tight text-gray-900">{product.name}</h1>
+				{/* Product info */}
+				<div className="mt-4 md:mt-0 px-5 pt-8 bg-white">
+					<h1 className="text-3xl font-bold tracking-tight text-gray-900">{product.name}</h1>
 
-							<div className="mt-3">
-								<h2 className="sr-only">Product information</h2>
-								{/* <Price price={product.price} regularPrice={product.regularPrice} /> */}
-							</div>
-
-							<div className="mt-6">
-								<h3 className="sr-only">Description</h3>
-								<div
-									className="space-y-6 text-base text-gray-700"
-									dangerouslySetInnerHTML={{ __html: product.description }}
-								/>
-							</div>
-
-							<form className="mt-6">
-								<div className="mt-10 flex gap-2">
-									<AuctionUp
-										id={product.id}
-										defaultValue={product?.auctions[0]?.mount}
-										step={50}
-									/>
-									<HeartButton
-										id={product.id}
-										className="p-3 text-blue-400 border-2 border-blue-400 rounded-md hover:text-blue-500 hover:bg-blue-100"/>
-									<ShareButton
-										id={product.id}
-										className="p-3 text-blue-400 border-2 border-blue-400 rounded-md hover:text-blue-500 hover:bg-blue-100"/>
-								</div>
-								<div className="mt-5 flex gap-2 flex-col">
-									<h3 className="font-bold">Ultimas ofertas:</h3>
-									<div className="bg-gradient-to-b from-indigo-500">
-										<div className="bg-white ml-1 pl-4 py-1 flex flex-col gap-3">
-											{ product.auctions.map( (auction, index) => <span key={`auction-${index}`} className={"flex flex-row cursor-pointer gap-2 hover:opacity-100 " + opacities[index] }>
-												<span className="flex flex-row gap-1">
-													<UserIcon />
-													<span className="text-indigo-500 font-semibold">{ hideEmail( auction?.user?.email ) }</span>
-												</span>
-												<span>ofertó <span className="font-semibold text-indigo-500">{auction.mount}$</span></span>
-												<span className="flex flex-row italic flex-nowrap">hace {auction.left}</span>
-											</span>) }
-										</div>
-									</div>
-								</div>
-							</form>
-
-							<section aria-labelledby="details-heading" className="mt-12">
-								<h2 id="details-heading" className="sr-only">
-									Detalles Adicionales
-								</h2>
-
-								<div className="divide-y divide-gray-200 border-t">
-									{/* {product?.details?.map((detail) => <Details key={detail.name} name={detail.name} items={detail.items}/> )} */}
-								</div>
-							</section>
-						</div>
+					<div className="mt-3">
+						<h2 className="sr-only">Product information</h2>
+						{/* <Price price={product.price} regularPrice={product.regularPrice} /> */}
 					</div>
 
-					{ product?.relatedProducts?.length>0 && <section aria-labelledby="related-heading" className="mt-10 border-t border-gray-200 px-4 py-16 sm:px-0">
-						<h2 id="related-heading" className="text-xl font-bold text-gray-900">
-							Productos Relacionados
+					<div className="mt-6">
+						<h3 className="sr-only">Description</h3>
+						<div
+							className="space-y-6 text-base text-gray-700"
+							dangerouslySetInnerHTML={{ __html: product.description }}
+						/>
+					</div>
+
+					<form className="mt-6">
+						<div className="mt-10 flex gap-2">
+							<AuctionUp
+								id={product.id}
+								defaultValue={product?.auctions?.at(0)?.mount}
+								step={50}
+							/>
+							<HeartButton
+								id={product.id}
+								className="p-3 text-blue-400 border-2 border-blue-400 rounded-md hover:text-blue-500 hover:bg-blue-100"/>
+							<ShareButton
+								id={product.id}
+								className="p-3 text-blue-400 border-2 border-blue-400 rounded-md hover:text-blue-500 hover:bg-blue-100"/>
+						</div>
+						{ ( product?.auctions && product.auctions?.length!==0) && <div className="mt-5 flex gap-2 flex-col">
+							<h3 className="font-bold">Ultimas ofertas:</h3>
+							<div className="bg-gradient-to-b from-indigo-500">
+								<div className="bg-white ml-1 pl-4 py-1 flex flex-col gap-3">
+									{ product.auctions?.map( (auction, index) => <span key={`auction-${index}`} className={"flex flex-row cursor-pointer gap-2 hover:opacity-100 " + opacities[index] }>
+										<span className="flex flex-row gap-1">
+											<UserIcon />
+											<span className="text-indigo-500 font-semibold">{ hideEmail( auction?.user?.email ) }</span>
+										</span>
+										<span>ofertó <span className="font-semibold text-indigo-500">{auction.mount}$</span></span>
+										<span className="flex flex-row italic flex-nowrap">hace {auction.left}</span>
+									</span>) }
+								</div>
+							</div>
+						</div>}
+					</form>
+
+					<section aria-labelledby="details-heading" className="mt-12">
+						<h2 id="details-heading" className="sr-only">
+							Detalles Adicionales
 						</h2>
 
-						<div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-							{/* {product.relatedProducts?.map((product) => <CardRelatedProduct key={product.id} product={product} /> )} */}
+						<div className="divide-y divide-gray-200 border-t">
+							{/* {product?.details?.map((detail) => <Details key={detail.name} name={detail.name} items={detail.items}/> )} */}
 						</div>
-					</section>}
+					</section>
 				</div>
-				<ShareModal url={"https://heroicons.com/"+product.id} />
-			</main>
+			</div>
+
+			{ (product?.relatedProducts && product?.relatedProducts?.length>0) && <section aria-labelledby="related-heading" className="mt-10 border-t border-gray-200 px-4 py-16 sm:px-0">
+				<h2 id="related-heading" className="text-xl font-bold text-gray-900">
+					Productos Relacionados
+				</h2>
+
+				<div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
+					{/* {product.relatedProducts?.map((product) => <CardRelatedProduct key={product.id} product={product} /> )} */}
+				</div>
+			</section>}
 		</div>
-	)
+		<ShareModal url={"https://heroicons.com/"+product.id} />
+	</main>
 }
 
 
