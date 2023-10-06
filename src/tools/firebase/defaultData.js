728bdc6c62ab67e339cf060379c16d45f68c53e6
Octavio Martinez <omartinez1618@gmail.com>
2023-10-06 17:31:07 -0400

    data default

---
+++ src/tools/firebase/defaultData.js
@@ -0,0 +1,25 @@
+const defaultData = {
+    products: {
+        name:'(name)',
+        description: '(description)',
+        auctions: [],
+        from:'',
+        to:'',
+        dimensions:{
+            width:0,
+            height: 0,
+            large:0,
+            units: 'mts'
+        },
+        status:'',
+        weight:'',
+        images: [{
+            name: "Default",
+            src: '/images/image-prueba.jpg'
+        }]
+
+        
+    }
+}
+
+export default defaultData;
