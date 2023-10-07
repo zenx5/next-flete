a9031dfc5cba348d7f2677e5d051bfb369b96a1f
Octavio Martinez <omartinez1618@gmail.com>
2023-10-07 10:42:21 -0400

    ajustes productos

--- src/components/product/details/ProductDetail.jsx
+++ src/components/product/details/ProductDetail.jsx
@@ -1,16 +1,13 @@
 "use client";
-import ImageSelector from "./ImageSelector"
-import AddToCart from '../AddToCart'
-import AuctionUp from '../AuctionUp'
-import Price from '../Price';
-import Details from './Details';
-import CardRelatedProduct from '../../CardRelatedProduct';
-import HeartButton from '../HeartButton';
-import ShareButton from '../ShareButton';
-import ShareModal from './ShareModal';
 import { useState, useEffect } from 'react';
 import { onSnap } from '@/tools/firebase/actions';
 import moment from "moment";
+import ImageSelector from "./ImageSelector"
+import AuctionUp from '../AuctionUp'
+import HeartButton from '../HeartButton';
+import ShareButton from '../ShareButton';
+import ShareModal from './ShareModal';
+import { UserIcon } from "../../icons";
 
 const opacities = [
 	"opacity-80",
@@ -101,12 +98,6 @@ export default function ProductDetail({ productId }) {
 				{/* Product info */}
 				<div className="mt-4 md:mt-0 px-5 pt-8 bg-white">
 					<h1 className="text-3xl font-bold tracking-tight text-gray-900">{product.name}</h1>
-
-					<div className="mt-3">
-						<h2 className="sr-only">Product information</h2>
-						{/* <Price price={product.price} regularPrice={product.regularPrice} /> */}
-					</div>
-
 					<div className="mt-6">
 						<h3 className="sr-only">Description</h3>
 						<div
@@ -158,22 +149,7 @@ export default function ProductDetail({ productId }) {
 					</section>
 				</div>
 			</div>
-
-			{ (product?.relatedProducts && product?.relatedProducts?.length>0) && <section aria-labelledby="related-heading" className="mt-10 border-t border-gray-200 px-4 py-16 sm:px-0">
-				<h2 id="related-heading" className="text-xl font-bold text-gray-900">
-					Productos Relacionados
-				</h2>
-
-				<div className="mt-8 grid grid-cols-1 gap-y-12 sm:grid-cols-2 sm:gap-x-6 lg:grid-cols-4 xl:gap-x-8">
-					{/* {product.relatedProducts?.map((product) => <CardRelatedProduct key={product.id} product={product} /> )} */}
-				</div>
-			</section>}
 		</div>
 		<ShareModal url={"https://heroicons.com/"+product.id} />
 	</main>
 }
-
-
-const UserIcon = () => <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-indigo-500">
-	<path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
-</svg>
