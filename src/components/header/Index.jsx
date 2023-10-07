3c6b6b683cce6aacd960eff94913c272c4917e55
Octavio Martinez <omartinez1618@gmail.com>
2023-10-07 13:30:57 -0400

    header

--- src/components/header/Index.jsx
+++ src/components/header/Index.jsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import ButtonCart from '../cart/ButtonCart';
 import MenuNav from './MenuNav';
 import Script from 'next/script';
+import Image from 'next/image';
 
 import { mainNavigation, secondaryNavigation } from '@/tools/navigation';
 import MovilMenu from './MovilMenu';
@@ -17,50 +18,29 @@ export default async function CustomHeader() {
     const mainNav = mainNavigation.filter( item => {
         if( isAuthenticatedBool ) { // Si esta autenticado
             return item.id!==ROUTER_ID.LOGIN
-        } else { // Si no esta autenticado
-            return item.id!==ROUTER_ID.PROFILE && item.id!==ROUTER_ID.LOGOUT && item.id!==ROUTER_ID.PRODUCTS
         }
-    } ).map( item => item )
+        // Si no esta autenticado
+        return item.id!==ROUTER_ID.PROFILE && item.id!==ROUTER_ID.LOGOUT && item.id!==ROUTER_ID.PRODUCTS
+    } )
 
-    return <header className="relative z-10 bg-black">
-        <nav aria-label="Top">
-            <div className="bg-white bg-opacity-10 backdrop-blur-md backdrop-filter">
-                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-                    <div>
-                        <div className="flex h-16 items-center justify-between">
-                            <div className="hidden lg:flex lg:flex-1 lg:items-center">
-                                <Link href="/">
-                                    <span className="text-lg font-bold text-white">CadeteSiempre</span>
-                                </Link>
-                            </div>
+    const accessNav = secondaryNavigation.filter( item => {
+        if( isAuthenticatedBool ) {
+            return item.id!==ROUTER_ID.LOGIN && item.id!==ROUTER_ID.REGISTER
+        }
+        return item.id!==ROUTER_ID.LOGOUT
+    })
 
-                            <div className="hidden h-full lg:flex">
-                                <div className="inset-x-0 bottom-0 px-4">
-                                    <div className="flex h-full justify-center space-x-8">
-                                        <MenuNav navigation={mainNav} />
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div className="flex flex-1 items-center lg:hidden">
-                                <MovilMenu />
-                            </div>
-
-                            <div className="flex flex-1 items-center justify-end gap-3">
-                                <Link href="/consulta" className="text-white block lg:hidden">
-                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
-                                    </svg>
-                                </Link>
-                                <MenuNav navigation={secondaryNavigation} />
-                                <div className="ml-4 flow-root lg:ml-8">
-                                    <ButtonCart />
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    return <header className="">
+        <div className="flex flex-row items-center gap-10 w-10/12 mx-auto justify-between">
+            <div>
+                <Image src="/images/logo.jpeg" alt="" width={250} height={200} />
             </div>
-        </nav>
+            <div className="hidden h-full lg:flex justify-center space-x-8 items-center">
+                <MenuNav navigation={mainNav} />
+            </div>
+            <div className="hidden h-full lg:flex justify-center space-x-8 items-center">
+                <MenuNav navigation={accessNav} />
+            </div>
+        </div>
     </header>
 }
