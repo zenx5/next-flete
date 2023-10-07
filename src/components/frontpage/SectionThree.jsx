b31ab075deaab70aeb66718d1ad4918be90723ba
Octavio Martinez <omartinez1618@gmail.com>
2023-10-07 13:30:00 -0400

    ajustes home

--- src/components/frontpage/SectionThree.jsx
+++ src/components/frontpage/SectionThree.jsx
@@ -1,8 +1,8 @@
+import Image from "next/image"
 
 export default function SectionThree() {
 
-	return (
-        <div className="grid grid-cols-1 md:grid-cols-2 bg-white pt-2 md:pt-8 pb-12 md:pb-16">
+	return <div className="grid grid-cols-1 md:grid-cols-2 bg-white pt-2 md:pt-8 pb-12 md:pb-16">
         <div className="flex flex-col pt-12 md:pt-16">
         <div className="px-14 md:px-20 py-4"><p className="text-3xl">Sobre Nosotros</p></div>
         <div className="px-14 md:px-20">
@@ -17,13 +17,14 @@ export default function SectionThree() {
          </div>
         </div>
         <div className="flex flex-col items-center pt-3 ">
-        <img
-        src="https://www.comparapps.com/wp-content/uploads/2020/03/imagenes-para-paginas-web.png"
-        alt=""
-        className="h-full w-11/12 object-cover object-center"
-      />
+          <Image
+            src="https://www.comparapps.com/wp-content/uploads/2020/03/imagenes-para-paginas-web.png"
+            alt=""
+            width={100}
+            height={100}
+            className="h-full w-11/12 object-cover object-center"
+          />
       </div>
-      </div>
-    )
+    </div>
 
 }
