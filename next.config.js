fcd1e4e418bcba876119cc8069d25a25c2924bb4
Octavio Martinez <omartinez1618@gmail.com>
2023-10-11 22:02:40 -0400

    agregado dominio para las imagenes

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ["localhost", "fastly.picsum.photos", "picsum.photos", "tailwindui.com", "detallesorballo.com", "www.comparapps.com"],
+        domains: ["localhost", "fastly.picsum.photos", "picsum.photos", "tailwindui.com", "detallesorballo.com", "www.comparapps.com","images.unsplash.com"],
     },
     experimental: {
         serverActions: true,
