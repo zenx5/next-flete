c743ad94488f6f514e108aeefef526c8991c3217
Octavio Martinez <omartinez1618@gmail.com>
2023-10-11 22:08:11 -0400

    icono con props

--- src/components/icons.jsx
+++ src/components/icons.jsx
@@ -4,7 +4,7 @@ export const ArrowLeft = () => <svg className="w-8 h-8" focusable="false" aria-h
 
 export const ArrowRight = () => <svg className="w-8 h-8" focusable="false" aria-hidden="true" viewBox="0 0 24 24"><path d="M8.59 16.34l4.58-4.59-4.58-4.59L10 5.75l6 6-6 6z"></path></svg>
 
-export const UserIcon = () => <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-indigo-500">
+export const UserIcon = (props) => <svg fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 text-indigo-500" {...props}>
 	<path strokeLinecap="round" strokeLinejoin="round" d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z" />
 </svg>
 
