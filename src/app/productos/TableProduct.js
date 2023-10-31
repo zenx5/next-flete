ccd37c160bf7285784e0c98e705d82de225dc13f
Octavio Martinez <omartinez1618@gmail.com>
2023-10-30 22:41:38 -0400

    ajustes del responsive

--- src/app/productos/TableProduct.js
+++ src/app/productos/TableProduct.js
@@ -84,19 +84,24 @@ export default function TableProduct({ userId, isAdmin }) {
 
 
 
-    return <div>
-        <div className="flex flex-row items-center gap-4 justify-between">
-            <div className="flex flex-row items-center border border-gray-200 hover:border-gray-900 rounded-lg py-2 px-4 my-5 gap-5 bg-white w-10/12">
+    return <div className="w-full md:w-10/12 px-5 mx-auto mt-5">
+        <div className="flex md:flex-row flex-col items-center gap-1 md:gap-4 justify-between w-full">
+            <div className="flex flex-row items-center border border-gray-200 hover:border-gray-900 rounded-lg py-2 px-4 my-5 gap-5 bg-white w-full md:w-10/12">
                 <SearchIcon />
                 <input type="text" value={target} onChange={event=>setTarget(event.target.value)} className="outline-none p-2 w-full" placeholder="Buscar..."/>
             </div>
-            <Link href={`?modal=edit-auction&params=id,userid&id=0&userid=${userId}`} className="bg-orange-500 hover:bg-orange-700 text-white rounded p-2 w-2/12 text-center">Crear Envio</Link>
+            <Link href={`?modal=edit-auction&params=id,userid&id=0&userid=${userId}`} className="md:relative fixed md:right-auto md:bottom-auto right-5 bottom-5 bg-orange-500 hover:bg-orange-700 text-white md:rounded rounded-full p-2 md:w-2/12 w-fit text-center">
+                <span className="md:block hidden ">Crear Envio</span>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="block md:hidden w-6 h-6">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+                </svg>
+            </Link>
         </div>
         <Tabs onChange={tab=>setCurrentTab(tab)} labels={ labelTabs.map( (label, index) => `${label} (${entities.filter( filterByTab(index) ).length})` ) }/>
-        <table className="table w-full border-spacing-0 border-collapse">
+        <table className="table w-full border-spacing-0 border-collapse px-2 mt-5">
             <thead>
-                <tr className="table-row align-middle outline-0">
-                    { fields.map( (field,index) => <th key={index} className="leading-6 text-sm font-semibold table-cell text-left text-[#637381] bg-[#F4F6F8] p-4  border-b-0">{ field.label }</th> )}
+                <tr className="hidden md:table-row align-middle outline-0">
+                    { fields.map( (field,index) => <th key={index} className="table-cell leading-6 text-sm font-semibold text-left text-[#637381] bg-[#F4F6F8] p-4  border-b-0">{ field.label }</th> )}
                     <th className="leading-6 text-sm font-semibold table-cell text-left text-[#637381] bg-[#F4F6F8] p-4  border-b-0">Actions</th>
                 </tr>
             </thead>
@@ -109,9 +114,9 @@ export default function TableProduct({ userId, isAdmin }) {
                 </tr>
             </tbody> }
         </table>
-        <div className="flex flex-row items-center justify-end gap-10">
-            <div className="p-4 flex flex-row gap-2">
-                <span>Rows per page</span>
+        <div className="flex flex-row items-center justify-end gap-10 md:relative fixed bottom-5 left-5">
+            <div className="md:p-4 p-0 flex flex-row gap-2">
+                <span className="md:block hidden">Rows per page</span>
                 <select className="outline-none" onChange={event=> setPerPage(event.target.value)} value={perPage}>
                     <option value={5}>5</option>
                     <option value={10}>10</option>
@@ -119,10 +124,10 @@ export default function TableProduct({ userId, isAdmin }) {
                     <option value={total}>All</option>
                 </select>
             </div>
-            <div className="p-4">
+            <div className="p-4 md:block hidden">
                 {min} - {max} of { total }
             </div>
-            <div className="flex flex-row p-4 items-center">
+            <div className="flex flex-row md:p-4 p-0 items-center">
                 <button onClick={() => setPage( prev - 1 )} disabled={page===1} className="disabled:text-gray-500"><ArrowLeft /></button>
                 <select className="font-semibold px-4 py-1 mx-2 border border-gray-400 rounded" value={page} onChange={ev => setPage( ev.target.value )}>
                     { Array(Math.round( total/perPage + 0.5 )).fill(1).map( (f, index) => <option key={`page-index-${index+1}`} value={index + 1}>{index + 1}</option> )}
