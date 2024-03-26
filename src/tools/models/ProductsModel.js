da97706dddb62299bcb1c61348d078f669d81cdf
mois452b <moisesgabrielrodriguezruiz@gmail.com>
2024-03-26 16:33:17 -0400

    refactoring changeStatus method, replace this.put for actionSave

--- src/tools/models/ProductsModel.js
+++ src/tools/models/ProductsModel.js
@@ -11,38 +11,22 @@ export default class ProductsModel extends BaseModel {
     }
 
     static async chagneStatus(id, status) {
-        if( status===STATUS.ACCEPT ) {
-            const data = await this.get(id)
-            if( data.status!==STATUS.ACTIVE ) return false
-            const index = data?.assignAt ? data?.assignAt?.index+1 : 0
+        const data = await this.get(id);
+        if (status === STATUS.ACCEPT) {
+            if( data.status !== STATUS.ACTIVE ) return false;
+            const index = data?.assignAt ? data?.assignAt?.index + 1 : 0;
             if( data?.auctions?.length > 0 ) {
-                const auctionsOrdered = data.auctions.sort(()=>-1)
-                return await this.put( id, {
-                    ...data,
-                    status,
-                    assignAt: {
-                        ...auctionsOrdered[index],
-                        index
-                    }
-                } )
+                const auctionsOrdered = data.auctions.sort(() => -1);
+                const newAssignAt = { ...auctionsOrdered[index], index };
+                return await actionSave( this.tableName, { ...data, status, assignAt: newAssignAt }, id );
             }
-            return false
-        }
-        else if( status===STATUS.CLOSED ) {
-            const data = await this.get(id)
-            if( data.status===STATUS.ACCEPT ) return false
-            return await this.put( id, {
-                ...data,
-                status
-            } )
-        }
-        else if( status===STATUS.ACTIVE ) {
-            const data = await this.get(id)
-            if( data.status===STATUS.ACCEPT ) return false
-            return await this.put( id, {
-                ...data,
-                status:status
-            } )
+            return false;
+        } else if( status === STATUS.CLOSED) {
+            if( data.status === STATUS.ACCEPT ) return false;
+            return await actionSave( this.tableName, { ...data, status }, id ) ;
+        } else if( status === STATUS.ACTIVE ) {
+            if( data.status === STATUS.ACCEPT ) return false;
+            return await actionSave( this.tableName, { ...data, status: status }, id );
         }
     }
 }
