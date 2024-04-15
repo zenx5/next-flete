9a21d167a926745594ef604cde0a58a55e4670f2
mois452b <moisesgabrielrodriguezruiz@gmail.com>
2024-04-15 16:39:40 -0400

    ussue 62 change color

--- src/app/usuario/(log)/layout.js
+++ src/app/usuario/(log)/layout.js
@@ -6,13 +6,13 @@ export default async function Layout({ children }){
 
     return <main className="w-screen h-auto flex justify-center items-center py-10 bg-gray-50">
         <form
-            className="px-4 py-8 border-2 border-blue-400 rounded-md shadow-md shadow-gray-200 bg-white"
+            className="px-4 py-8 border-2 border-[#d97706] rounded-md shadow-md shadow-gray-200 bg-white"
             method="post"
             action={ROUTER_PATH.API.USER}
         >
             {children}
             <input type="hidden" name="redirect" value={ROUTER_PATH.HOME} />
-            <button type="submit" className="w-full text-white bg-blue-500 font-bold border border-blue-500 p-3 my-4 rounded hover:bg-blue-600">Enviar</button>
+            <button type="submit" className="w-full text-white bg-[#d97706] font-bold border border-[#d97706] p-3 my-4 rounded hover:bg-blue-600">Enviar</button>
             <p className="text-center text-red-600 italic">{error.message}</p>
         </form>
     </main>
