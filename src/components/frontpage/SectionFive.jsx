d883319c9944ad54020a1f541211cda7980a9109
mois452b <moisesgabrielrodriguezruiz@gmail.com>
2024-04-18 17:47:04 -0400

    fix bug

--- src/components/frontpage/SectionFive.jsx
+++ src/components/frontpage/SectionFive.jsx
@@ -1,6 +1,6 @@
 export default function SectionFive() {
 
-	return <section><div className="grid grid-cols-1 md:grid-cols-2 bg-zinc-600 h-[46rem] md:h-[28rem] text-white">
+	return <section><div className="grid grid-cols-1 md:grid-cols-2 bg-zinc-600 h-[46rem] md:max-h-[30rem] text-white">
       <div className="">
         <img
           src="/images/imagenes-para-paginas-web.png"
@@ -8,7 +8,7 @@ export default function SectionFive() {
           className="w-full h-full object-cover object-center" />
       </div>
       <div className="flex flex-col justify-center items-center">
-        <h3 className="text-5xl w-10/12 font-medium p-3">Selecciona tu transportista y precio</h3>
+        <h3 className="text-5xl w-10/12 font-medium py-3">Selecciona tu transportista y precio</h3>
         <p className="font-medium w-10/12 p-3">
           HayFlete le da la oportunidad de recibir el mejor precio al darle acceso
           a miles de transportistas que compiten por su negocio para enviar con confianza.
@@ -22,8 +22,8 @@ export default function SectionFive() {
       </div>
     </div>
 
-    <div className="grid grid-cols-1 md:grid-cols-2 bg-blue-900 h-[46rem] md:h-[28rem] text-white">
-        <div className="flex flex-col md:flex-col justify-center items-center">
+    <div className="grid grid-cols-1 md:grid-cols-2 bg-blue-900 h-[46rem] md:h-[30rem] text-white">
+        <div className="flex flex-col justify-center items-center">
           <h3 className="text-5xl w-10/12 font-medium py-3">Selecciona tu transportista y precio</h3>
           <p className="font-medium w-10/12 p-3">
             HayFlete le da la oportunidad de recibir el mejor precio al darle acceso
@@ -45,7 +45,7 @@ export default function SectionFive() {
 
       </div>
       
-      <div className="grid grid-cols-1 md:grid-cols-2 bg-black h-[46rem] md:h-[28rem] text-white">
+      <div className="grid grid-cols-1 md:grid-cols-2 bg-black h-[46rem] md:h-[30rem] text-white">
       <div className="">
         <img
           src="/images/imagenes-para-paginas-web.png"
