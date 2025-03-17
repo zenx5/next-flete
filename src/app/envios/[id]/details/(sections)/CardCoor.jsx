3f8ac0c478c461ae47396b6a020a68ba77e3ca76
Octavio Martinez <omartinez1618@gmail.com>
2025-03-17 09:43:04 -0400

    feat: Añade manejadores de eventos onMouseStart y onMouseEnd a CardCoor
    [by Kommiter]

--- src/app/envios/[id]/details/(sections)/CardCoor.jsx
+++ src/app/envios/[id]/details/(sections)/CardCoor.jsx
@@ -1,5 +1,10 @@
-export default function CardCoor({ location }) {
-    return <span className="col-span-1 p-2 flex flex-col items-center">
+"use client"
+
+export default function CardCoor({ location, onMouseStart, onMouseEnd }) {
+    const handleMouseUp = () => onMouseStart && onMouseStart()
+    const handleMouseLeave = () => onMouseEnd && onMouseEnd()
+
+    return <span className="col-span-1 p-2 flex flex-col items-center" onMouseUp={handleMouseUp} onMouseLeave={handleMouseLeave}>
         <span className="font-bold">{ location.name }</span>
         <span className="italic text-xs">({ location.position.lat }, { location.position.lng })</span>
     </span>
